73c7d28612ea711001f5d77cb6feaedb1b3999cb
afc163 <afc163@gmail.com>
2022-04-19 11:44:56 +0800

    fix pirture card removing motion

--- components/upload/style/motion.tsx
+++ components/upload/style/motion.tsx
@@ -25,29 +25,33 @@ const genMotionStyle: GenerateStyle<FullToken<'Upload'>> = (token, hashId) => {
   const { componentCls } = token;
   const inlineCls = `${componentCls}-animate-inline`;
 
-  return {
-    [`${componentCls}-wrapper`]: {
-      [`
+  return [
+    {
+      [`${componentCls}-wrapper`]: {
+        [`
         ${inlineCls}-appear,
         ${inlineCls}-enter,
         ${inlineCls}-leave
       `]: {
-        animationDuration: token.motionDurationSlow,
-        animationFillMode: token.motionEaseInOutCirc,
-      },
+          animationDuration: token.motionDurationSlow,
+          animationFillMode: token.motionEaseInOutCirc,
+        },
 
-      [`
+        [`
         ${inlineCls}-appear,
         ${inlineCls}-enter
       `]: {
-        animationName: `${uploadAnimateInlineIn.getName(hashId)}`,
-      },
+          animationName: `${uploadAnimateInlineIn.getName(hashId)}`,
+        },
 
-      [`${inlineCls}-leave`]: {
-        animationName: `${uploadAnimateInlineOut.getName(hashId)}`,
+        [`${inlineCls}-leave`]: {
+          animationName: `${uploadAnimateInlineOut.getName(hashId)}`,
+        },
       },
     },
-  };
+    uploadAnimateInlineIn,
+    uploadAnimateInlineOut,
+  ];
 };
 
 export default genMotionStyle;
